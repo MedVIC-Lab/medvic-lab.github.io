56cbb450bf564d13411fd8376327cbdcb4746a5f
Jake Wagoner <jakewagoneredu@gmail.com>
2025-01-13 11:34:49 -0700

    Add links and styles for accessing individual publication page

--- scripts/generatePublications.ts
+++ scripts/generatePublications.ts
@@ -3,6 +3,22 @@ import * as path from 'path';
 import fm from 'front-matter';
 import { Plugin } from 'vite';
 
+type Publication = {
+  title: string;
+  authors: string;
+  conference: string;
+  year: string;
+  image: string;
+  link: string; // local file link
+  links?: {
+    code?: string;
+    publisher?: string;
+    video?: string;
+    pdf?: string;
+    archive?: string;
+  };
+};
+
 const generatePublicationsPlugin = (): Plugin => {
   return {
     name: 'vite-plugin-generate-publications',
@@ -10,19 +26,28 @@ const generatePublicationsPlugin = (): Plugin => {
       const publicationsDir = path.join(__dirname, '../pages/publications');
       const outputFilePath = path.join(__dirname, '../public/assets/publications.json');
 
-      const publications: Array<{ title: string; authors: string; conference: string; year: string; link: string; image: string }> = [];
+      const publications: Publication[] = [];
 
       fs.readdirSync(publicationsDir).forEach(file => {
         const filePath = path.join(publicationsDir, file);
         const fileContent = fs.readFileSync(filePath, 'utf-8');
         const data = fm(fileContent);
 
+        data.attributes['links'] = data.attributes['links'] || {};
+
         publications.push({
           title: data.attributes.title || '',
           authors: data.attributes.authors || '',
           conference: data.attributes.conference || '',
           year: data.attributes.year || '',
-          link: data.attributes.link || `/pages/publications/${file.replace('.md', '')}`,
+          link: `/pages/publications/${file.replace('.md', '')}`,
+          links: {
+            code: data.attributes.links.code || '',
+            publisher: data.attributes.links.publisher || '',
+            video: data.attributes.links.video || '',
+            pdf: data.attributes.links.pdf || '',
+            archive: data.attributes.links.archive || ''
+          },
           image: data.attributes.image || ''
         });
       });
